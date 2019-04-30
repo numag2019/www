2d2aa181ca23bbb3bc8f6563af4c8a6de896df59
Amaury Branthomme <amaury.branthomme@agro-bordeaux.fr>
2019-04-30 16:54:03 +0200

    Update fiche_race.php

--- fpdf181/exports/fiche_race.php
+++ fpdf181/exports/fiche_race.php
@@ -163,7 +163,7 @@ function FancyTable($header, $data)
 
 
 //Fonction réalisant le tableau
-function Tableau($header,$effectif,$largeur_col,$largeur_lgd)
+function Tableau_inv($header,$effectif,$largeur_col,$largeur_lgd)
 {
     // Couleurs, épaisseur du trait et police grasse pour l'entete
 	$this->SetFillColor(255,0,0);
@@ -212,45 +212,194 @@ function Tableau($header,$effectif,$largeur_col,$largeur_lgd)
 
 }
 
+function Tableau_presence($header,$effectif,$largeur_col)
+{
+    // Couleurs, épaisseur du trait et police grasse pour l'entete
+	$this->SetFillColor(255,0,0);
+	$this->SetTextColor(0);
+	$this->SetDrawColor(128,0,0);
+	$this->SetLineWidth(.3);
+	$this->SetFont('','B');
+    $this->SetFontSize(7);    
+    
+	// En-tête
+	foreach($header as $col)
+        $this->Cell($largeur_col,7,$col,1,0,'C',true);
+	$this->Ln();
+    
+    
+	// Données
+	foreach($effectif as $row)
+	{
+        foreach($row as $col)
+        {
+            // Restauration des couleurs et de la police pour les données du tableau
+            $this->SetFillColor(224,235,255);
+            $this->SetTextColor(0);
+            $this->SetFont('');
+            $this->SetFontSize(7);
+            $this->Cell($largeur_col,6,$col,'LR',0,'C');
+            
+            
+        }
+		$this->Ln();
+	}
+    // Trait de terminaison
+	$this->Cell(count($header)*$largeur_col,0,'','T'); //trait pour fermer le tableau
+
 }
 
+
+
+//Fonction réalisant le tableau
+function Tableau_nais($header,$effectif,$largeur_col,$largeur_lgd)
+{
+    // Couleurs, épaisseur du trait et police grasse pour l'entete
+	$this->SetFillColor(255,0,0);
+	$this->SetTextColor(0);
+	$this->SetDrawColor(128,0,0);
+	$this->SetLineWidth(.3);
+	$this->SetFont('','B');
+    
+	// En-tête
+	foreach($header as $col)
+        if (is_null($col))
+            $this->Cell($largeur_lgd,7,$col,1,0,'C',true);//Si la case est la légende, on applique un style particulier
+        else
+            $this->Cell($largeur_col,7,$col,1,0,'C',true); 
+	$this->Ln();
+    
+    
+	// Données
+	foreach($effectif as $row)
+	{
+        foreach($row as $col)
+        {
+            if (is_numeric($col))
+            {
+                // Restauration des couleurs et de la police pour les données du tableau
+                $this->SetFillColor(224,235,255);
+                $this->SetTextColor(0);
+                $this->SetFont('');
+                $this->SetFontSize(13);
+                $this->Cell($largeur_col,6,$col,'LR',0,'C');
+            }
+            else//Si la case est la légende, on applique un style particulier
+            {
+                // Restauration des couleurs et de la police pour les données du tableau
+                $this->SetFillColor(224,235,255);
+                $this->SetTextColor(0);
+                $this->SetFont('');
+                $this->SetFontSize(8);
+                $this->Cell($largeur_lgd,6,$col,'LR',0,'C'); 
+            }
+        }
+		$this->Ln();
+	}
+    
+
+}
+
+//Fonction réalisant le tableau
+function Tableau_nais_2($header,$effectif,$largeur_col,$largeur_lgd)
+{
+    // Couleurs, épaisseur du trait et police grasse pour l'entete
+	$this->SetFillColor(255,0,0);
+	$this->SetTextColor(0);
+	$this->SetDrawColor(128,0,0);
+	$this->SetLineWidth(.3);
+	$this->SetFont('','B');
+    
+	// En-tête
+	foreach($header as $col)
+        if (is_null($col))
+            $this->Cell($largeur_lgd,7,$col,1,0,'C',true);//Si la case est la légende, on applique un style particulier
+        else
+            $this->Cell($largeur_col,7,$col,1,0,'C',true); 
+	$this->Ln();
+    
+    
+	// Données
+	foreach($effectif as $row)
+	{
+        foreach($row as $col)
+        {
+            if (is_numeric($col))
+            {
+                // Restauration des couleurs et de la police pour les données du tableau
+                $this->SetFillColor(224,235,255);
+                $this->SetTextColor(0);
+                $this->SetFont('');
+                $this->SetFontSize(13);
+                $this->Cell($largeur_col,6,$col,'LR',0,'C');
+            }
+            else//Si la case est la légende, on applique un style particulier
+            {
+                // Restauration des couleurs et de la police pour les données du tableau
+                $this->SetFillColor(224,235,255);
+                $this->SetTextColor(0);
+                $this->SetFont('');
+                $this->SetFontSize(8);
+                $this->Cell($largeur_lgd,6,$col,'LR',0,'C'); 
+            }
+        }
+		$this->Ln();
+	}
+    // Trait de terminaison
+	$this->Cell($largeur_lgd+(count($header)-1)*$largeur_col,0,'','T'); //trait pour fermer le tableau
+    
+
+}
+}
+
+
+
+
+///////////////////////////////////////////fin des fonctions ///////////////////////////////////////////////////
+
+//écriture des pages PDF
 $pdf = new PDF();
+
 // Titres des colonnes
-$header_inv = array('',2013,2014,2015,2016,2017);
+$header_inv_nais = array(NULL,2013,2014,2015,2016,2017);
+$header_pre = array('Nom','N° id','Elevage','Sexe','Date naissance','Nom du père','N° id père','Nom de la mère','N° id mère','Naisseur');
+$header_nais2 = array(NULL,'Nb','%','Nb','%','Nb','%','Nb','%','Nb','%');
+
 // Données des requetes SQL
 $effectif = array(array('Total des femelles inventoriées',252,286,318,352,375),array('Femelles de plus de 2ans',193,209,234,253,264),array('Femelles nées et conservées',38,38,42,59,56),array('Taureaux (MN)',5,8,10,12,8),array('Détenteurs',65,75,75,82,92));
+$presence = array(array('Idole',1234567890,'Pierre','F','2013-0-22','Arcachon',1234567890,'Cascaille',1234567890,'Pierre'),array('Julie',1234567890,'Pierre','F','2013-0-22','Arcachon',1234567890,'Cascaille',1234567890,'Pierre'),array('Justine',1234567890,'Pierre','F','2013-0-22','Arcachon',1234567890,'Cascaille',1234567890,'Pierre'),array('Hilda',1234567890,'Pierre','F','2013-0-22','Arcachon',1234567890,'Cascaille',1234567890,'Pierre'),array('Hermine',1234567890,'Pierre','F','2013-0-22','Arcachon',1234567890,'Cascaille',1234567890,'Pierre'));
+$naissance1 = array(array('nombre de veaux nés',118,130,141,159,164));
+$naissance2 = array(array('Veaux mâles','55','46','73','56','70','50','81','51','84','51'),array('Veaux femelles','63','54','57','44','71','50','78','49','80','49'));
 
-//remplissage des pages
+//Page des tableaux
 $pdf->AliasNbPages(); //nécessaire pour afficher le nombre de pages
 $pdf->AddPage();
 $pdf->SetFont('');
 
 //Tableau d'évolution des effectifs inventories dans la race
-$pdf->Tableau($header_inv,$effectif,30,40);
+$pdf->Tableau_inv($header_inv_nais,$effectif,30,40);
 
 //Espace entre les différents éléments de la page
-$pdf->Ln(40);
+$pdf->Ln(10);
+
+//Tableau d'évolution des naissances
+$pdf->Tableau_nais($header_inv_nais,$naissance1,30,40);
+$pdf->Tableau_nais_2($header_nais2,$naissance2,15,40);
+
+//Espace entre les différents éléments de la page
+$pdf->Ln(10);
+
+//Tableau d'évolution de la présence dans la race
+$pdf->Tableau_presence($header_pre,$presence,19);
+
+// Page des graphiques
+$pdf->AddPage();
 
 //Graphique d'évolution des effectifs
 $pdf->Cell(70,120,'Graphique',1,0,'C');
 
-//Espace entre les différents éléments de la page
-$pdf->Ln(40);
-
-//Tableau d'évolution des naissances
-$pdf->Tableau($header_inv,$effectif,30,40);
-
-//Espace entre les différents éléments de la page
-$pdf->Ln(40);
-
 //Graphique d'évolution des naissances
 $pdf->Cell(70,120,'Graphique',1,0,'C');
-
-//Espace entre les différents éléments de la page
-$pdf->Ln(40);
-
-//Tableau d'évolution de la présence dans la race
-$pdf->Tableau($header_inv,$effectif,30,40);
 
 $pdf->Output();
 ?>
