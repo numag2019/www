3ca6d32419058b8a55323bff390c01dfac66e89f
vincentlebg <guillaume.vincent@agro-bordeaux.fr>
2019-05-03 11:50:23 +0200

    Graph Updated

--- graph/HistoEvoBorn.php
+++ graph/HistoEvoBorn.php
@@ -1,10 +1,6 @@
 <?php
 
 session_start();
-//
-// Problème de couleur de légende sa mère la pute
-//
-
 
 //Exemple
 require_once ('./jpgraph-4.2.6/src/jpgraph.php');
@@ -16,8 +12,9 @@ $datax2=array(65,70,60,50,40,30);
 $années=array("2014","2015","2016","2017","2018","2019");
 
 /*
-$datax=$_GET("-_Insérer le nom de vos données en abscisse_-");
-$datay=$_GET("-_Insérer le nom de vos données en ordonnée_-");
+$datax1=$_GET["-_Insérer les naissances de mâles en ordonnée issu des requêtes_-"];
+$datax2=$_GET["-_Insérer les naissances de femelles en ordonnée issu des requêtes_-"];
+$années=$_GET["-_Insérer les vaches nées et conservées en ordonnée issu des requêtes_-"];
 */
 
 // *********************
@@ -27,14 +24,16 @@ $datay=$_GET("-_Insérer le nom de vos données en ordonnée_-");
 $graph = new Graph(640,480);    
 $graph->SetScale("textlin");
 
-$graph->SetMargin(50,65,20,40);
+$graph->SetMargin(50,65,50,40);
 
 // Désactiver le cadre autour du graphique
 $graph->SetFrame(false);
 
 // Ajouter un onglet
-$graph->tabtitle->Set("Effectif des femelles");
-$graph->tabtitle->SetFont(FF_ARIAL,FS_BOLD,10);
+$graph->tabtitle->Set("Evolution des naissances");
+$graph->tabtitle->SetFont(FF_ARIAL,FS_BOLD,14);
+$graph->tabtitle->SetColor("black");
+$graph->tabtitle->SetFillColor("#E9EBF3");
 
 // Apparence des grilles
 $graph->ygrid->SetFill(true,'#DDDDDD@0.5','#BBBBBB@0.5');
@@ -45,7 +44,6 @@ $graph->xgrid->SetLineStyle('dashed');
 $graph->xgrid->SetColor('gray');
 $graph->xaxis->setTickLabels($années);
 $graph->xaxis->setLabelAngle(50);
-$graph->legend->Pos(0.12,0.12,"right","top");
 
 // *******************************
 // Créer un histogramme
@@ -55,8 +53,7 @@ $graph->legend->Pos(0.12,0.12,"right","top");
 	
 	$histo_male = new barPlot($datax2);
 	$histo_male->value->SetFormat('%d');
-	$histo_male->SetFillGradient('#AFE0EC', '#AFE0EC', GRAD_LEFT_REFLECTION);
-	$histo_male->SetLegend('Femelles vivantes');
+	$histo_male->SetLegend('Mâles nés');
 	// Changer la taille//  $histo_male->SetWidth(valeur);
 	$histo_male->SetWeight(0);
 	
@@ -64,9 +61,9 @@ $graph->legend->Pos(0.12,0.12,"right","top");
 	////Second histo////
 	
 	$histo_fem = new BarPlot($datax1);
-	$histo_fem->SetLegend('Femelles mortes');
+	$histo_fem->SetLegend('Femelles nées');
 	$histo_fem->value->Show();
-	$histo_fem->SetFillGradient('#3F67DC', '#3F67DC', GRAD_LEFT_REFLECTION);
+
 	$histo_fem->value->SetFormat('%d');
 	$histo_fem->SetWeight(0);
 
@@ -79,16 +76,21 @@ $graph->legend->Pos(0.12,0.12,"right","top");
 	$gbplot->value->SetFont(FF_COMIC,FS_NORMAL,8);
 	$gbplot->value->SetFormat('%d');
 
+	
+
+//Nom des axes
+$graph->yaxis->title->Set("Nombre de naissances");
+$graph->xaxis->title->Set("Années");
 
 // Position de la légende
-$graph->legend->Pos(0.20,0.12,"left","top");
-$graph->SetYScale(0,'lin', 0,0);
+$graph->legend->Pos(0.10,0.05);
 
 // Ajouter l'ensemble accumulé
 $graph->Add($gbplot);
-	
+	$histo_fem->SetFillColor('#6078E5');
+	$histo_male->SetFillColor('#A5E4F2');
 
 // Envoyer au navigateur
 $graph->Stroke();
-$graph->Stroke("Graphique.jpg");
+$graph->Stroke("EvoNaissances.png");
 ?>
