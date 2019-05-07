a9b5af00b3dc1e55e960882cf3648cd17066542f
Amaury Branthomme <amaury.branthomme@agro-bordeaux.fr>
2019-05-07 10:33:06 +0200

    Update fiche_race.php

--- genis.cra/fpdf181/exports/fiche_race.php
+++ genis.cra/fpdf181/exports/fiche_race.php
@@ -1,10 +1,11 @@
 <?php
-//cette page à pour but de coder l'exportation en pdf de l'export Fiche Race. Cet export est composé de 3 tableaux et de 2 graphiques
-//élève référent : Amaury Branthomme
+//cette page Ã  pour but de coder l'exportation en pdf de l'export Fiche Race. Cet export est composÃ© de 3 tableaux et de 2 graphiques
+//Ã©lÃ¨ve rÃ©fÃ©rent : Amaury Branthomme
 
 session_start();
 
-//récupération des variables de session
+
+//rÃ©cupÃ©ration des variables de session
 $nb_femelle = $_SESSION['nb_femelle'];
 $nb_femelle_2 =$_SESSION['nb_femelle_2'];
 $nb_femelle_nee = $_SESSION['nb_femelle_nee'];
@@ -24,14 +25,14 @@ protected $I = 0;
 protected $U = 0;
 protected $HREF = '';
 
-// En-tête
+// En-tÃªte
 function Header()
 {
 	//Logo
 	$this->Image('logo.jpg',10,6,30,0,'','http://racesaquitaine.fr/');
 	//Police Arial gras 15
 	$this->SetFont('Arial','B',15);
-	//Décalage à droite
+	//DÃ©calage Ã  droite
 	$this->Cell(80);
 	//Titre
 	$this->Cell(70,10,'Voici le titre de mon entete',1,0,'C');
@@ -42,15 +43,15 @@ function Header()
 // Pied de page
 function Footer()
 {
-	// Positionnement à 1,5 cm du bas
+	// Positionnement Ã  1,5 cm du bas
 	$this->SetY(-15);
 	// Police Arial italique 8
 	$this->SetFont('Arial','I',8);
-	// Numéro de page
+	// NumÃ©ro de page
 	$this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
 }
 
-// Chargement des données
+// Chargement des donnÃ©es
 function LoadData($file)
 {
 	// Lecture des lignes du fichier
@@ -120,7 +121,7 @@ function CloseTag($tag)
 
 function SetStyle($tag, $enable)
 {
-	// Modifie le style et sélectionne la police correspondante
+	// Modifie le style et sÃ©lectionne la police correspondante
 	$this->$tag += ($enable ? 1 : -1);
 	$style = '';
 	foreach(array('B', 'I', 'U') as $s)
@@ -142,47 +143,47 @@ function PutLink($URL, $txt)
 }
 
 
-//Fonction réalisant le tableau
+//Fonction rÃ©alisant le tableau
 function Tableau_inv($header,$effectif,$largeur_col,$largeur_lgd)
 {
-    // Couleurs, épaisseur du trait et police grasse pour l'entete
+    // Couleurs, Ã©paisseur du trait et police grasse pour l'entete
 	$this->SetFillColor(133,195,43);
 	$this->SetTextColor(0);
 	$this->SetDrawColor(0,0,0); //couleur des lignes du tableau
 	$this->SetLineWidth(.3);
 	$this->SetFont('','B');
     
-	// En-tête
+	// En-tÃªte
 	foreach($header as $col)
         if (is_numeric($col))
             $this->Cell($largeur_col,7,$col,1,0,'C',true);
         else
-            $this->Cell($largeur_lgd,7,$col,1,0,'C',true); //Si la case est la légende, on applique un style particulier
+            $this->Cell($largeur_lgd,7,$col,1,0,'C',true); //Si la case est la lÃ©gende, on applique un style particulier
 	$this->Ln();
     
     
-	// Données
+	// DonnÃ©es
 	foreach($effectif as $row)
 	{
         foreach($row as $col)
         {
             if (is_numeric($col))
             {
-                // Restauration des couleurs et de la police pour les données du tableau
+                // Restauration des couleurs et de la police pour les donnÃ©es du tableau
                 $this->SetFillColor(224,235,255);
                 $this->SetTextColor(0);
                 $this->SetFont('');
                 $this->SetFontSize(13);
                 $this->Cell($largeur_col,6,$col,'LR',0,'C');
             }
-            else//Si la case est la légende, on applique un style particulier
+            else//Si la case est la lÃ©gende, on applique un style particulier
             {
-                // Restauration des couleurs et de la police pour les données du tableau
+                // Restauration des couleurs et de la police pour les donnÃ©es du tableau
                 $this->SetFillColor(224,235,255);
                 $this->SetTextColor(0);
                 $this->SetFont('');
                 $this->SetFontSize(8);
-                $this->Cell($largeur_lgd,6,$col,'LR',0,'C'); 
+                $this->Cell($largeur_lgd,6,utf8_decode($col),'LR',0,'C'); 
             }
         }
 		$this->Ln();
@@ -194,7 +195,7 @@ function Tableau_inv($header,$effectif,$largeur_col,$largeur_lgd)
 
 function Tableau_presence($header,$effectif,$largeur_col)
 {
-    // Couleurs, épaisseur du trait et police grasse pour l'entete
+    // Couleurs, Ã©paisseur du trait et police grasse pour l'entete
 	$this->SetFillColor(133,195,43);
 	$this->SetTextColor(0);
 	$this->SetDrawColor(0,0,0); //couleur des lignes du tableau
@@ -202,18 +203,18 @@ function Tableau_presence($header,$effectif,$largeur_col)
 	$this->SetFont('','B');
     $this->SetFontSize(7);    
     
-	// En-tête
+	// En-tÃªte
 	foreach($header as $col)
         $this->Cell($largeur_col,7,$col,1,0,'C',true);
 	$this->Ln();
     
     
-	// Données
+	// DonnÃ©es
 	foreach($effectif as $row)
 	{
         foreach($row as $col)
         {
-            // Restauration des couleurs et de la police pour les données du tableau
+            // Restauration des couleurs et de la police pour les donnÃ©es du tableau
             $this->SetFillColor(224,235,255);
             $this->SetTextColor(0);
             $this->SetFont('');
@@ -230,42 +231,42 @@ function Tableau_presence($header,$effectif,$largeur_col)
 }
 
 
-//Fonction réalisant le tableau
+//Fonction rÃ©alisant le tableau
 function Tableau_nais($header,$effectif,$largeur_col,$largeur_lgd)
 {
-    // Couleurs, épaisseur du trait et police grasse pour l'entete
+    // Couleurs, Ã©paisseur du trait et police grasse pour l'entete
 	$this->SetFillColor(133,195,43);
 	$this->SetTextColor(0);
 	$this->SetDrawColor(0,0,0); //couleur des lignes du tableau
 	$this->SetLineWidth(.3);
 	$this->SetFont('','B');
     
-	// En-tête
+	// En-tÃªte
 	foreach($header as $col)
         if (is_null($col))
-            $this->Cell($largeur_lgd,7,$col,1,0,'C',true);//Si la case est la légende, on applique un style particulier
+            $this->Cell($largeur_lgd,7,$col,1,0,'C',true);//Si la case est la lÃ©gende, on applique un style particulier
         else
             $this->Cell($largeur_col,7,$col,1,0,'C',true); 
 	$this->Ln();
     
     
-	// Données
+	// DonnÃ©es
 	foreach($effectif as $row)
 	{
         foreach($row as $col)
         {
             if (is_numeric($col))
             {
-                // Restauration des couleurs et de la police pour les données du tableau
+                // Restauration des couleurs et de la police pour les donnÃ©es du tableau
                 $this->SetFillColor(224,235,255);
                 $this->SetTextColor(0);
                 $this->SetFont('');
                 $this->SetFontSize(13);
                 $this->Cell($largeur_col,6,$col,'LR',0,'C');
             }
-            else//Si la case est la légende, on applique un style particulier
+            else//Si la case est la lÃ©gende, on applique un style particulier
             {
-                // Restauration des couleurs et de la police pour les données du tableau
+                // Restauration des couleurs et de la police pour les donnÃ©es du tableau
                 $this->SetFillColor(224,235,255);
                 $this->SetTextColor(0);
                 $this->SetFont('');
@@ -279,42 +280,42 @@ function Tableau_nais($header,$effectif,$largeur_col,$largeur_lgd)
 
 }
 
-//Fonction réalisant le tableau
+//Fonction rÃ©alisant le tableau
 function Tableau_nais_2($header,$effectif,$largeur_col,$largeur_lgd)
 {
-    // Couleurs, épaisseur du trait et police grasse pour l'entete
+    // Couleurs, Ã©paisseur du trait et police grasse pour l'entete
 	$this->SetFillColor(133,195,43);
 	$this->SetTextColor(0);
 	$this->SetDrawColor(0,0,0);
 	$this->SetLineWidth(.3);
 	$this->SetFont('','B');
     
-	// En-tête
+	// En-tÃªte
 	foreach($header as $col)
         if (is_null($col))
-            $this->Cell($largeur_lgd,7,$col,1,0,'C',true);//Si la case est la légende, on applique un style particulier
+            $this->Cell($largeur_lgd,7,$col,1,0,'C',true);//Si la case est la lÃ©gende, on applique un style particulier
         else
             $this->Cell($largeur_col,7,$col,1,0,'C',true); 
 	$this->Ln();
     
     
-	// Données
+	// DonnÃ©es
 	foreach($effectif as $row)
 	{
         foreach($row as $col)
         {
             if (is_numeric($col))
             {
-                // Restauration des couleurs et de la police pour les données du tableau
+                // Restauration des couleurs et de la police pour les donnÃ©es du tableau
                 $this->SetFillColor(224,235,255);
                 $this->SetTextColor(0);
                 $this->SetFont('');
                 $this->SetFontSize(13);
                 $this->Cell($largeur_col,6,$col,'LR',0,'C');
             }
-            else//Si la case est la légende, on applique un style particulier
+            else//Si la case est la lÃ©gende, on applique un style particulier
             {
-                // Restauration des couleurs et de la police pour les données du tableau
+                // Restauration des couleurs et de la police pour les donnÃ©es du tableau
                 $this->SetFillColor(224,235,255);
                 $this->SetTextColor(0);
                 $this->SetFont('');
@@ -336,56 +337,66 @@ function Tableau_nais_2($header,$effectif,$largeur_col,$largeur_lgd)
 
 ///////////////////////////////////////////fin des fonctions ///////////////////////////////////////////////////
 
-//écriture des pages PDF
+//Ã©criture des pages PDF
 $pdf = new PDF();
 
 // Titres des colonnes des tableaux
-//création de l'entete des années
+//crÃ©ation de l'entete des annÃ©es
 $header_inv_nais = array();
 array_push($header_inv_nais,NULL);
 for($i=0;$i<count($annee);$i++) 
     array_push($header_inv_nais,$annee[$i]);
 
 
-$header_pre = array('Nom','N° id','Elevage','Sexe','Date naissance','Nom du père','N° id père','Nom de la mère','N° id mère','Naisseur');
+$header_pre = array('Nom','NÂ° id','Elevage','Sexe','Date naissance','Nom du pÃ¨re','NÂ° id pÃ¨re','Nom de la mÃ¨re','NÂ° id mÃ¨re','Naisseur');
 
-//création de l'entete Nb et %
+//crÃ©ation de l'entete Nb et %
 $header_nais2 = array();
 array_push($header_nais2,NULL);
 for($i=0;$i<count($header_inv_nais)-1;$i++) 
     array_push($header_nais2,"Nb","%");
 
 
-// Données des requetes SQL
+// DonnÃ©es des requetes SQL
 
-//$effectif = array(array('Total des femelles inventoriées',252,286,318,352,375),array('Femelles de plus de 2ans',193,209,234,253,264),array('Femelles nées et conservées',38,38,42,59,56),array('Taureaux (MN)',5,8,10,12,8),array('Détenteurs',65,75,75,82,92));
+//$effectif = array(array('Total des femelles inventoriÃ©es',252,286,318,352,375),array('Femelles de plus de 2ans',193,209,234,253,264),array('Femelles nÃ©es et conservÃ©es',38,38,42,59,56),array('Taureaux (MN)',5,8,10,12,8),array('DÃ©tenteurs',65,75,75,82,92));
 $effectif = array($nb_femelle, $nb_femelle_2, $nb_femelle_nee, $nb_taureau, $nb_detenteur);
 $_SESSION['effectif'] = $effectif;
 
 
 
 $naissance1 = array($nb_veau);
-//$naissance2 = array(array('Veaux mâles','55','46','73','56','70','50','81','51','84','51'),array('Veaux femelles','63','54','57','44','71','50','78','49','80','49'));
+//$naissance2 = array(array('Veaux mÃ¢les','55','46','73','56','70','50','81','51','84','51'),array('Veaux femelles','63','54','57','44','71','50','78','49','80','49'));
 
 //Modification de la variable nb_veau_m et nb_veau_f pour ajouter les pourcentages
 
-//création des variables contenant les pourcentages
+//crÃ©ation des variables contenant les pourcentages
 $pourcent_veaux_m = array();
 $pourcent_veaux_f = array();
-for($i=1;$i<count($nb_veau)-1;$i++)
+
+for($i=1;$i<count($nb_veau);$i++)
 {
-    $pourcent_veaux_m[$i] = intval(($nb_veau_m[$i]/$nb_veau[$i])*100);
-    $pourcent_veaux_f[$i] = intval(($nb_veau_f[$i]/$nb_veau[$i])*100);
+    $pourcent_veaux_m[$i-1] = intval(($nb_veau_m[$i]/$nb_veau[$i])*100);
+    $pourcent_veaux_f[$i-1] = intval(($nb_veau_f[$i]/$nb_veau[$i])*100);
 }
 
-//création d'une variable contenant le nombre et le pourcentage des veaux
-$veaux_m = array();
-$veaux_f = array();
+//crÃ©ation d'une variable contenant le nombre et le pourcentage des veaux
 
-for($i=1;$i<count($nb_veau)-1;$i++)
+$veaux_m =array($nb_veau_m[0]);
+$veaux_f =array($nb_veau_f[0]);
+
+$j=1;
+
+for($i=1;$i<count($nb_veau);$i++)
 {
-    $veaux_m[$i] = [$nb_veau_m[$i];$pourcent_veaux_m[$i]];
-    $veaux_f[$i] = [$nb_veau_m[$i];$pourcent_veaux_f[$i]];
+    $veaux_m[$j] = $nb_veau_m[$i];
+    $veaux_f[$j] = $nb_veau_f[$i];
+    $j++;
+    // $k = $j-2;
+    $veaux_m[$j] = $pourcent_veaux_m[$i-1];
+    $veaux_f[$j] = $pourcent_veaux_f[$i-1];
+    $j = $j+1;    
+    
 }
 
 
@@ -398,35 +409,35 @@ $presence = array(array('Idole',1234567890,'Pierre','F','2013-0-22','Arcachon',1
 
 
 //Page des tableaux
-$pdf->AliasNbPages(); //nécessaire pour afficher le nombre de pages
+$pdf->AliasNbPages(); //nÃ©cessaire pour afficher le nombre de pages
 $pdf->AddPage();
 $pdf->SetFont('');
 
 //Taille des colonnes
-$largeur_col = 150/(count($header_inv_nais)-1); //taille des colonnes des années adaptatives en fonction du nombre d'années
+$largeur_col = 150/(count($header_inv_nais)-1); //taille des colonnes des annÃ©es adaptatives en fonction du nombre d'annÃ©es
 $largeur_lgd = 40;
 
-//Tableau d'évolution des effectifs inventories dans la race
+//Tableau d'Ã©volution des effectifs inventories dans la race
 $pdf->Tableau_inv($header_inv_nais,$effectif,$largeur_col,$largeur_lgd);
 
 
-//Graphique d'évolution des effectifs
+//Graphique d'Ã©volution des effectifs
 //$pdf->Image('../../graph/EvoNbFem.png',7,100,-80);
 
 //Page des naissances
 $pdf->AddPage();
-//Tableau d'évolution des naissances
+//Tableau d'Ã©volution des naissances
 
 $pdf->Tableau_nais($header_inv_nais,$naissance1,$largeur_col,$largeur_lgd);
 $pdf->Tableau_nais_2($header_nais2,$naissance2,$largeur_col/2,$largeur_lgd);
 
-//Graphique d'évolution des naissances
+//Graphique d'Ã©volution des naissances
 //$pdf->Image('../../graph/EvoNaissances.png',7,100,-80);
 
 
-// Page d'évolution des présences dans la race
+// Page d'Ã©volution des prÃ©sences dans la race
 $pdf->AddPage();
-//Tableau d'évolution de la présence dans la race
+//Tableau d'Ã©volution de la prÃ©sence dans la race
 $pdf->Tableau_presence($header_pre,$presence,19);
 
 
