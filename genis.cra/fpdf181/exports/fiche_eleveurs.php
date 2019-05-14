e7101cb3300e7b8912efd2104fa10e2a38e005f5
Amaury Branthomme <amaury.branthomme@agro-bordeaux.fr>
2019-05-14 09:08:56 +0200

    kjebzc

--- genis.cra/fpdf181/exports/fiche_eleveurs.php
+++ genis.cra/fpdf181/exports/fiche_eleveurs.php
@@ -178,7 +178,8 @@ for($i=1;$i<=2;$i++)
 	    if($i==1)
 	    {
 		//sauvegarde du fichier
-		$pdf->Output('../../exportation/pdf/fiche_eleveur_'.$race.'.pdf','F');
+		// $pdf->Output('../../exportation/pdf/fiche_eleveur_'.$race.'.pdf','F');
+        $pdf->Output('../../exportation/pdf/fiche_eleveur_'.$race.'_'.$annee_ele.'.pdf','F'); //Pour afficher l'année dans le titre des pdf
 	    }
 	    else
 	    {
